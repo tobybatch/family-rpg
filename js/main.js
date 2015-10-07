e625a75fb13c1059fc6c73d7004781a17814122e
Toby Batch <tobias@neontribe.co.uk>
2015-10-07 20:54:40 +0100

    Attributes work

--- js/main.js
+++ js/main.js
@@ -14,11 +14,72 @@ $(function() {
     });
   });
 
+  function lookupValue(value) {
+    if (10 === value) {
+      return 'Weakest';
+    }
+    if (20 === value) {
+      return 'Poor';
+    }
+    if (30 === value) {
+      return 'Average';
+    }
+    if (40 === value) {
+      return 'Good';
+    }
+    if (50 === value) {
+      return 'Superhuman';
+    }
+
+    return 'Err: Unknown';
+  }
+  
   function evaluateAttributes() {
+    var total = 0;
     $( ".slider" ).each(function() {
       var slider = this;
       var value = $(slider).slider('option', 'value');
-        console.log($(slider).data('name') + ': ' + value);
-      });
+      total += value;
+      // console.log($(slider).data('name') + ': ' + value);
+      var txtval = lookupValue(value);
+      $(slider).parent().find('.attval').html(txtval);
+    });
+    total = total/10;
+    $('#total .attval').html(total);
+    $('#total .attval').removeClass('under');
+    $('#total .attval').removeClass('on');
+    $('#total .attval').removeClass('over');
+    if (18 == total) {
+      $('#total .attval').addClass('on');
+    }
+    if (18 < total) {
+      $('#total .attval').addClass('over');
+    }
+    if (18 > total) {
+      $('#total .attval').addClass('under');
+    }
   }
+  
+
+  function lookupValue(value) {
+      if (10 === value) {
+          return 'Weakest';
+      }
+      if (20 === value) {
+          return 'Poor';
+      }
+      if (30 === value) {
+          return 'Average';
+      }
+      if (40 === value) {
+          return 'Good';
+      }
+      if (50 === value) {
+          return 'Superhuman';
+      }
+
+      return 'Err: Unknown';
+  }
+
+  evaluateAttributes();
 });
